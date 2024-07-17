32ac66ec8bbf4565c21b754c74c17b81028d62b4
Abigail Syanwa <47054909+Syanwa@users.noreply.github.com>
2024-07-17 15:08:01 +0300

    first commit

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function abbysawesome_script_enqueue(){
+wp_enqueue_style
+('customstlyle',get_template_directory_uri().'wp-content/themes/abbysawesometheme2024/css/abbysawesome.css', 
+array(),'1.0.0','all');
+
+wp_enqueue_script
+('customsjs',get_template_directory_uri().'wp-content/themes/abbysawesometheme2024/js/abbysawrsome.jss', 
+array(),'1.0.0',true);
+/*good for page loading speed to print true for js file.
+*/
+}
+
+add_action('wp_enqueue_scripts', 'abbysawesome_script_enqueue');
+/* This action was included to include my custom js and custom css.
+*/
+
+function abbysawesome_theme_setup(){
+
+    add_theme_support('menus');
+
+    register_nav_menu('primary','Header navigation');
+    register_nav_menu('secondary','Footer navigation');
+
+}
+add_action('init','abbysawesome_theme_setup');
+/* This requests wordpress to execute the function after initialization. The other option is after_setup_theme
+*/
